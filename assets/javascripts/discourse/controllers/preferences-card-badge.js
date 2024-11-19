7bba4ed59c5767d96aba321042273905744aff3f
David Taylor <david@taylorhq.com>
2024-11-19 10:32:23 +0000

    DEV: Update eslint config (#78)

--- assets/javascripts/discourse/controllers/preferences-card-badge.js
+++ assets/javascripts/discourse/controllers/preferences-card-badge.js
@@ -1,6 +1,6 @@
 import Controller from "@ember/controller";
 import EmberObject from "@ember/object";
-import { inject as service } from "@ember/service";
+import { service } from "@ember/service";
 import { ajax } from "discourse/lib/ajax";
 import Badge from "discourse/models/badge";
 import computed from "discourse-common/utils/decorators";
