e0bed6c4235fca9e765fd85d12b31bd6a4d8ed4c
Jarek Radosz <jradosz@gmail.com>
2026-01-28 12:53:35 +0100

    DEV: Update linting and add types (#105)
    * DEV: Update linting and add types
    
    * DEV: Update linting and add types

--- spec/controllers/user_card_badge_controller_spec.rb
+++ spec/controllers/user_card_badge_controller_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 describe UserCardBadges::UserCardBadgeController do
   routes { UserCardBadges::Engine.routes }
 
--- spec/plugin_spec.rb
+++ spec/plugin_spec.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
 describe ::UserCardBadges do
   let(:user) { Fabricate(:user) }
   let(:badge) { Fabricate(:badge) }
